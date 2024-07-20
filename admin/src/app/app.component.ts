76870ae6344f8dd713aaf31779e29785dd0e4309
Johan-Romo <jaromo3@espe.edu.ec>
2024-07-20 10:57:47 -0500

    subida 2

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
+import { IndexClienteComponent } from './components/clientes/index-cliente/index-cliente.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { EditClienteComponent } from './components/clientes/edit-cliente/edit-cliente.component';
+import { CreateProductoComponent } from './components/productos/create-producto/create-producto.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,9 +18,14 @@ import { LoginComponent } from './components/login/login.component';
      RouterLink,
      RouterLinkActive,
      RouterModule,
+     FormsModule,
+     SidebarComponent,
+     LoginComponent,
      InicioComponent,
-     LoginComponent
-
+     IndexClienteComponent,
+     NgbPaginationModule,
+     EditClienteComponent,
+     CreateProductoComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
